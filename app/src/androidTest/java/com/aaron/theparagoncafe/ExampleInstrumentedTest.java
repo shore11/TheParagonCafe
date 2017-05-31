e656a8ff6e4cee0829cbc87d4f6258607697f80f
cameronlyman <clyman97@yahoo.com>
2017-05-31 13:29:38 -0600

    This is a test to see if I have git set up correctly.

--- app/src/androidTest/java/com/aaron/theparagoncafe/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/aaron/theparagoncafe/ExampleInstrumentedTest.java
@@ -13,6 +13,8 @@ import static org.junit.Assert.*;
  * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ *
+ * HELLO WORLD! THIS IS A TEST FOR GIT.
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
