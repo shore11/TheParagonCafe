08fa385bf9fda7226dce49dcca3bcb7f99d984b4
Jaredbyu <bro15032@byui.edu>
2017-06-07 17:58:56 -0600

    added a drawable folder so we can have backround images

--- app/src/main/java/com/aaron/theparagoncafe/MainActivity.java
+++ app/src/main/java/com/aaron/theparagoncafe/MainActivity.java
@@ -1,7 +1,11 @@
 package com.aaron.theparagoncafe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void sendMessage(View view) {
+
+
+
+        String tag = "sendMessage";
+
+
+        Intent intent = new Intent(this, MenuActivity.class);
+
+        startActivity(intent);
+        // Do something in response to button
+    }
+
 }
