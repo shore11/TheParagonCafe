5930ca94f35a8a8ebf1d21579f5f18235c8c8662
Aaron <twin11shore@gmail.com>
2017-06-09 13:53:37 -0600

    Added Firebase access to menu activity along with a firebase listener for data change

--- app/src/main/java/com/aaron/theparagoncafe/MenuActivity.java
+++ app/src/main/java/com/aaron/theparagoncafe/MenuActivity.java
@@ -1,50 +1,84 @@
 package com.aaron.theparagoncafe;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
+
 public class MenuActivity extends AppCompatActivity {
+
+    // variable to access items in the database
+    private DatabaseReference mDatabaseRefrence;
+    // gets a reference to the specific database as a whole
+    private FirebaseDatabase mFirebaseDatabase;
+    // when items in the database change this can update the fields
+    private ChildEventListener mChildEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        Food food1 = new Food("Ham");
-        Food food2 = new Food("Soup");
-        Food food3 = new Food("Cheese");
+        // this gets the root node of the database
+        mDatabaseRefrence = mFirebaseDatabase.getReference();
+        // if you want access to a specific node
+        // string cheesbuger = mDatabaseRefrence.getReference().child("keyofchild")
+        // to go farther down the tree  child().child().child()
 
-        Food[] values = new Food[] {food1, food2, food3};
+
+        String[] values = new String[] { "Soup", "Meat", "Crackers", "Ramen"};
 
         final ArrayList<String> list = new ArrayList<String>();
         for (int i = 0; i < values.length; ++i) {
-            list.add(values[i].getName());
+            list.add(values[i]);
         }
 
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
-                list);
+                values);
 
         ListView listview = (ListView) findViewById(R.id.foodList);
 
         listview.setAdapter(adapter);
 
+        // when things change in the data base these functions handle that
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
 
-    }
+            }
 
-    public void createFoodActivity(View view) {
-String name = "HELLO";
-        Intent intent = new Intent(this, Food.class);
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
-        startActivity(intent);
-        // Do something in response to button
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mDatabaseRefrence.addChildEventListener(mChildEventListener);
     }
 }
